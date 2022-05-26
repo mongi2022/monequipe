1a93006d2f52a49840f6e9a9d7c1cecfdf9f96cc
mongi22 <mongi.thabet@tekniva.net>
2022-05-26 09:31:49 +0100

    first commit

--- src/app/core/joueurs/joueurs.service.ts
+++ src/app/core/joueurs/joueurs.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Joueurs } from './joueurs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class JoueursService {
+  joueurs!:Joueurs[]
+  baseApi!:''
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+  getAllJoueurs():Observable{
+return this._http.get()
+  }
 }
--- src/app/core/joueurs/joueurs.ts
+++ src/app/core/joueurs/joueurs.ts
@@ -1,2 +1,12 @@
 export class Joueurs {
+  id: number;
+  name: string;
+  email: string;
+  adresse: string;
+  constructor(id: number, name: string, email: string, adresse: string) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.adresse = adresse;
+  }
 }
